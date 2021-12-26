8cc349c2a5a53af77d41a19e8651aab941570a56
omerisk <96565192+omerisk@users.noreply.github.com>
2021-12-26 20:40:31 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>İyi ki Doğdun</title>
+</head>
+<body>
+ 
+<h1>İYİ Kİ DOĞDUN SEVGİLİM</h1>
+<p>Bu da benim ilk paragrafım. Merhaba HTML. Merhaba Dünya</p>
+
+<a href="https://www.youtube.com/watch?v=zbxJJN1tOw0/" target="blank">Anasayfaya Gidin.</a>
+ 
+</body>
+</html>
